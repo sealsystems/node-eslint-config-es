05ac2d78fb493d2a4b87158d3b17716d79d2d789
Michael Scherer <michael.scherer@sealsystems.de>
2018-11-30 17:10:00 +0100

    Add dependencies

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 'use strict';
 
 module.exports = {
-  extends: 'prettier',
-  parserOptions: {
-    ecmaVersion: 6
+  "extends": [
+    "prettier"
+  ],
+  "plugins": [
+    "prettier"
+  ],
+  "env": {
+    "es6": true,
+    "node": true
   },
-  rules: {
-    'id-length': [0, {
-      min: 2,
-      max: Number.infinity,
-      properties: 'always',
-      exceptions: ['_']
-    }],
-    'max-params': [1, { max: 5 }],
-    'max-statements': [1, { max: 100 }],
-    'max-len': [1, {
-      code: 200,
-      tabWidth: 2,
-      comments: 200,
-      ignoreComments: false,
-      ignoreTrailingComments: false,
-      ignoreUrls: true
-    }],
-    quotes: [ 2, 'single', { avoidEscape: false, allowTemplateLiterals: true }]
+  "rules": {
+    "prettier/prettier": ["error", {
+      "singleQuote": true,
+      "parser": "flow",
+      "arrowParens": "always"
+    }]
   }
 };

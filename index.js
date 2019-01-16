3c214a2261cb77c5924460fa18ae6d748a8cb59e
seal-mt <manfred.thein@sealsystems.de>
2019-01-16 16:54:31 +0100

    enable es6 environment and some eslint rules

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ module.exports = {
   ],
   "env": {
     "node": true,
-    "mocha": true
+    "mocha": true,
+    "es6": true
   },
   "parserOptions": {
     "ecmaVersion": 2017
@@ -19,10 +20,15 @@ module.exports = {
     "prettier/prettier": ["error", {
       "singleQuote": true,
       "parser": "flow",
-      "arrowParens": "always"
+      "arrowParens": "always",
+      "printWidth": 120
     }],
     "no-console": "error",
     "no-unused-vars": "error",
+    "no-undef": "error",
+    "no-dupe-keys": "error",
+    "no-duplicate-case": "error",
+    "no-unreachable": "error",
     "mocha/no-exclusive-tests": "error"
   }
 };
